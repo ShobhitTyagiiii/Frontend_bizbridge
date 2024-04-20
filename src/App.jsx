3134e4f27e6c6d95a269f497b8196e2cd1ed3ab0
OmSharma532004 <22103203@mail.jiit.ac.in>
2024-04-20 14:26:19 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=' bg-green-600 text-white w-full min-h-screen'>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
